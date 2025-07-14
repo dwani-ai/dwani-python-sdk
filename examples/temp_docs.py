4177803334551348e1b2df2638f6968f137ac513
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-07-14 17:16:34 +0200

    status

--- examples/temp_docs.py
+++ examples/temp_docs.py
@@ -5,107 +5,24 @@ import os
 dwani.api_key = os.getenv("DWANI_API_KEY")
 dwani.api_base = os.getenv("DWANI_API_BASE_URL")
 
-'''
 
 result = dwani.Documents.run_ocr_number(
             file_path="olmocr-paper.pdf", model="gemma3", page_number=5
         )
 
-
-print("Document Query Response: gemma3- ", result)
-
-print("------------")
-result = dwani.Documents.run_ocr_number(
-            file_path="olmocr-paper.pdf", model="gemma3", page_number=2
-        )
-
-print("Document Query Response: gemma3- ", result)
-
-print("------------")
-
-result = dwani.Documents.run_ocr_number(
-            file_path="olmocr-paper.pdf", model="gemma3", page_number=3
-        )
-
 print("Document Query Response: gemma3- ", result)
 
 print("------------")
 result = dwani.Documents.run_ocr_all(
             file_path="dwani-ai-pitch-deck.pdf", model="gemma3"
         )
-
-
-result = dwani.Documents.run_ocr_all(
-            file_path="dwani-workshop.pdf", model="gemma3"
-        )
-
-result = dwani.Documents.run_ocr_all(
-            file_path="dwani-ai-pitch-deck.pdf", model="gemma3"
-        )
-print("Document Query Response: gemma3- ", result)
-
-'''
-
-'''
+print("------------")
 
 result = dwani.Documents.summarize_page(
             file_path="dwani-ai-pitch-deck.pdf", model="gemma3" , page_number= 1
         )
 print("Document Query Response: gemma3- ", result)
-
-
-
-result = dwani.Documents.summarize_page(
-            file_path="dwani-ai-pitch-deck.pdf", model="gemma3" , page_number= 3, tgt_lang="english"
-        )
-print("Document Query Response: gemma3- ", result)
-
-result = dwani.Documents.summarize_page(
-            file_path="dwani-ai-pitch-deck.pdf", model="gemma3" , page_number= 4 , tgt_lang="english"
-        )
-print("Document Query Response: gemma3- ", result)
-
-result = dwani.Documents.summarize_page(
-            file_path="dwani-ai-pitch-deck.pdf", model="gemma3" , page_number= 8 , tgt_lang="english"
-        )
-print("Document Query Response: gemma3- ", result)
-
-
-result = dwani.Documents.summarize_all(
-            file_path="dwani-ai-pitch-deck.pdf", model="gemma3" , tgt_lang="english"
-        )
-#print("Document Query Response: gemma3- ", result)
-
-#print("Document Query Response: gemma3- ", result["translated_summary"])
-
-
-print("Document Query Response: gemma3- ", result["summary"])
-
-result = dwani.Documents.summarize_all(
-            file_path="sample_resume.pdf", model="gemma3" , tgt_lang="english"
-  
-    )
-
-print("Document Query Response: gemma3- ", result["translated_summary"])
-
-#print("Document Query Response: gemma3- ", result)
-
-#print("Document Query Response: gemma3- ", result["summary"])
-
-#print("Document Query Response: gemma3- ", result["translated_summary"])
-
-
-#print("Document Query Response: gemma3- ", result["summary"])
-'''
-
-
-result = dwani.Documents.summarize_all(
-            file_path="dwani-workshop.pdf", model="gemma3" , tgt_lang="english"  
-    )
-
-
-print("Document Query Response: gemma3- ", result["summary"])
-
+print("------------")
 
 result = dwani.Documents.summarize_all(
             file_path="dwani-ai-pitch-deck.pdf", model="gemma3" , tgt_lang="english"
@@ -113,17 +30,4 @@ result = dwani.Documents.summarize_all(
 
 print("Document Query Response: gemma3- ", result["translated_summary"])
 
-result = dwani.Documents.summarize_all(
-            file_path="sample_resume.pdf", model="gemma3" , tgt_lang="english"
-  
-    )
 
-print("Document Query Response: gemma3- ", result["translated_summary"])
-
-
-result = dwani.Documents.summarize_all(
-            file_path="olmocr-paper.pdf", model="gemma3" , tgt_lang="english"  
-    )
-
-
-print("Document Query Response: gemma3- ", result["summary"])

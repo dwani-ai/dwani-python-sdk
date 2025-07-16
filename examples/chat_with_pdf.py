292c551e3d779bda29dcc6eb30f7bbe6db584645
sachin <26170834+sachinsshetty@users.noreply.github.com>
2025-07-16 13:40:26 +0200

    changes

--- examples/chat_with_pdf.py
+++ examples/chat_with_pdf.py
@@ -6,14 +6,17 @@ dwani.api_key = os.getenv("DWANI_API_KEY")
 dwani.api_base = os.getenv("DWANI_API_BASE_URL")
 
 
+'''
 result = dwani.Documents.query_page(
             file_path="olmocr-paper.pdf", model="gemma3", page_number=5
         )
 
 print("Document Query Response: gemma3- ", result)
-
+'''
 print("------------")
 result = dwani.Documents.query_all(
-            file_path="dwani-ai-pitch-deck.pdf", model="gemma3"
+            file_path="dwani-ai-pitch-deck.pdf", model="gemma3", tgt_lang="kan_Knda"
         )
 print("------------")
+
+print("Document Query Response: gemma3- ", result["translated_query_answer"])
